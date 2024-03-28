b5fe8cf2fdf8b0d2e8b7774bafe651f834dcc020
GuillaumeRobin9 <guirobin37@gmail.com>
2024-03-28 19:14:38 +0100

    docker clean add aws + default alpine add to scenario with ftp client on it

--- environment/choose_scenario_mode.py
+++ environment/choose_scenario_mode.py
@@ -25,7 +25,7 @@ def main():
 def deploy():
     print("Please choose the deployment mode:")
     print("1. Local")
-    print("2. aws")
+    print("2. AWS")
     mode_choice = input("Your choice: ")
 
     if mode_choice == "1":
@@ -39,11 +39,37 @@ def deploy():
     print(f"Deployment in {mode} mode in progress...")
 
     # Execute the Ansible playbook with the --vault-password-file option
-    subprocess.run(["ansible-playbook", "scenario1.yml", "-e", f"mode={mode}", "--ask-vault-pass"])
+    subprocess.run(["ansible-playbook", "scenario1_all_in_oneEC2.yml", "-e", f"mode={mode}", "--ask-vault-pass"])
 
 def remove_docker_containers():
     print("Removing Docker containers...")
-    subprocess.run(["ansible-playbook", "clean_docker.yml"])
+    print("Please choose the cleaning mode:")
+    print("1. Local")
+    print("2. AWS")
+    mode_choice = input("Your choice: ")
+
+    if mode_choice == "1":
+        mode = "local"
+    elif mode_choice == "2":
+        mode = "aws"
+        print("Would you like to clean all the docker or delete the EC2 instance ?")
+        print("1. Docker")
+        print("2. EC2")
+        second_choice = input("Your choice: ")
+        if second_choice == "1":
+            second_mode = "docker"
+        elif second_choice == "2":
+            second_mode = "ec2"
+        else:
+            print("Invalid choice. Please choose 1 or 2.")
+            return
+    else:
+        print("Invalid choice. Please choose 1 or 2.")
+        return
+
+    print(f"Cleaning in {mode} mode in progress...")
+
+    subprocess.run(["ansible-playbook", "clean_docker.yml", "-e", f"mode={mode}", "-e", f"second_mode={second_mode}", "--ask-vault-pass"])
 
 if __name__ == "__main__":
     main()
