3b8ab30fdaa6ff7f0d4803d4c9a1a419b6b64fb2
0nano <35236525+0nano@users.noreply.github.com>
2024-04-01 16:32:16 +0200

    services and deploy

---
+++ services/entry.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -n "$GW" ]; then
+	echo "Setting GW to $GW" 
+	/sbin/route del default 
+	/sbin/route add default gw $GW
+else
+	echo "GW env parameter not set"
+	echo "Keeping defaults"
+fi
+exec $@
