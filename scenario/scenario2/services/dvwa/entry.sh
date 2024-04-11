234ae145583b63513d8307b51a0565675d4fd0ce
GuillaumeRobin9 <guirobin37@gmail.com>
2024-04-11 10:54:44 +0200

    pas mal les changements

--- scenario/scenario2/services/dvwa/entry.sh
+++ scenario/scenario2/services/dvwa/entry.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 if [ -n "$GW" ]; then
-        echo "Setting GW to $GW"
-        /sbin/route del default
-        /sbin/route add default gw $GW
+	echo "Setting GW to $GW" 
+	/sbin/route del default 
+	/sbin/route add default gw $GW
 else
-        echo "GW env parameter not set"
-        echo "Keeping defaults"
-fiexec $@
+	echo "GW env parameter not set"
+	echo "Keeping defaults"
+fi
+exec $@
+
