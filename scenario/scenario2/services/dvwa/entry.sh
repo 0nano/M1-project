891ccd52976b9fe678347f10a3782fef61995cc5
GuillaumeRobin9 <guirobin37@gmail.com>
2024-04-11 09:09:16 +0200

    exec $@ remove

--- scenario/scenario2/services/dvwa/entry.sh
+++ scenario/scenario2/services/dvwa/entry.sh
@@ -7,4 +7,4 @@ else
 	echo "GW env parameter not set"
 	echo "Keeping defaults"
 fi
-exec $@
+#exec $@ # Aussi fou que ça soit cette ligne est considéré comme un fin de ligne Windows et donc en peut être executé sur debian 
