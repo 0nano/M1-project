12a8ec1bb57a1e7687617f0a3e640da481911af1
GuillaumeRobin9 <guirobin37@gmail.com>
2024-04-11 09:23:18 +0200

    perdu

--- scenario/scenario2/services/dvwa/entry.sh
+++ scenario/scenario2/services/dvwa/entry.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 if [ -n "$GW" ]; then
-	echo "Setting GW to $GW" 
-	/sbin/route del default 
-	/sbin/route add default gw $GW
+        echo "Setting GW to $GW"
+        /sbin/route del default
+        /sbin/route add default gw $GW
 else
-	echo "GW env parameter not set"
-	echo "Keeping defaults"
+        echo "GW env parameter not set"
+        echo "Keeping defaults"
 fi
-#exec $@ # Aussi fou que ça soit cette ligne est considéré comme un fin de ligne Windows et donc en peut être executé sur debian 
