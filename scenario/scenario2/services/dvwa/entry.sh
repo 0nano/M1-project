ec6d96a93a279957d2129634f0cbc41849e86141
GuillaumeRobin9 <guirobin37@gmail.com>
2024-04-11 11:14:12 +0200

    # Removing windows /r character

--- scenario/scenario2/services/dvwa/entry.sh
+++ scenario/scenario2/services/dvwa/entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -n "$GW" ]; then
-	echo "Setting GW to $GW" 
-	/sbin/route del default 
+	echo "Setting GW to $GW"
+	/sbin/route del default
 	/sbin/route add default gw $GW
 else
 	echo "GW env parameter not set"
