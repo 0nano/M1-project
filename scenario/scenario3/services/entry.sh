84decdc81ba1724067eec9d5aecc1b46abd9e775
0nano <35236525+0nano@users.noreply.github.com>
2024-04-16 13:11:33 +0200

    Update scenario3

--- scenario/scenario3/services/entry.sh
+++ scenario/scenario3/services/entry.sh
@@ -7,11 +7,4 @@ else
 	echo "GW env parameter not set"
 	echo "Keeping defaults"
 fi
-if [ -n "$DNS" ]; then
-	echo "Setting DNS to $DNS"
-	sed -i "s/nameserver */nameserver $DNS/" /etc/resolv.conf
-else
-	echo "DNS env parameter not set"
-	echo "Keeping defaults"
-fi
 exec $@
